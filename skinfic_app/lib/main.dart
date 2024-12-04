c6b9dd1b311ba05689ed23c7b736fbce88c16885
Natthawut Suksai <owen55660@gmail.com>
2024-12-04 13:07:13 +0700

    feat: change ScreenUtilInit from figma

--- skinfic_app/lib/main.dart
+++ skinfic_app/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(375, 812),
+      designSize: const Size(360, 800),
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (context, child) {
